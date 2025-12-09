c1ee19c5f2fc043dd4f5a1d5eafe26db241e7b9d
PeacefulKim <corbata@naver.com>
2025-12-09 16:41:25 +0900

    chore: 폰트 추가

--- KeyStorm Dungeon/Assets/01.Scripts/Managers/AudioManager.cs
+++ KeyStorm Dungeon/Assets/01.Scripts/Managers/AudioManager.cs
@@ -5,24 +5,40 @@ using UnityEngine;
 public class AudioManager : SingletonManager<AudioManager>
 {
     [SerializeField] AudioSource bgmAudio;
-    
+
+    [SerializeField] SoundDatas bgmData;
+    [SerializeField] SoundDatas sfxData;
+
     Dictionary<string, AudioClip> bgmClips = new Dictionary<string, AudioClip>();
     Dictionary<string, AudioClip> sfxClips = new Dictionary<string, AudioClip>();
+
+
 
     protected override void Awake()
     {
         base.Awake();
+        initAudioDic();
 
     }
     private void Start()
     {
-        
+        LoadVolume();
     }
 
-    void initAudioDic()
+    void initAudioDic() //사운드 초기화
     {
+        for(int i = 0; i<bgmData.audioClips.Count; i++)
+        {
+            AudioData audioData = bgmData.audioClips[i];
+            bgmClips.Add(audioData.audioKey, audioData.audioClip);
+        }
+        for (int i = 0; i < sfxData.audioClips.Count; i++)
+        {
+            AudioData audioData = sfxData.audioClips[i];
+            sfxClips.Add(audioData.audioKey, audioData.audioClip);
+        }
     }
-    void LoadVolume()
+    void LoadVolume() //볼륨값
     {
 
     }
---
+++ KeyStorm Dungeon/Assets/01.Scripts/Managers/SfxPoolManager.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class SfxPoolManager : SingletonManager<SfxPoolManager>
+{
+    [SerializeField] int poolSize;
+    
+
+
+}
