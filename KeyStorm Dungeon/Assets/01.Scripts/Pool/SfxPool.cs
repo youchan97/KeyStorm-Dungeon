39cbab3d22b2d10f21a55112de5b9f8dca04d151
PeacefulKim <corbata@naver.com>
2025-12-09 18:00:18 +0900

    feat: 오디오매니저 / SFX 풀링 추가
    작동이 잘 되는지는 아직 모르겠습니다...

---
+++ KeyStorm Dungeon/Assets/01.Scripts/Pool/SfxPool.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class SfxPool : MonoBehaviour
+{
+    [SerializeField] private AudioSource sfxAudio;
+    SfxPoolManager PoolManager;
+
+    private void Awake()
+    {
+        PoolManager = SfxPoolManager.Instance;
+    }
+    public void PlaySfx(AudioClip clip, float volume)
+    {
+        if (clip == null) return;
+        sfxAudio.clip = clip;
+        sfxAudio.volume = volume;
+        sfxAudio.Play();
+
+        StartCoroutine(WaitReturnPool());
+    }
+    IEnumerator WaitReturnPool()
+    {
+        yield return new WaitForSeconds(sfxAudio.clip.length);
+        PoolManager.ReturnObject(this);
+    }
+}
---
+++ KeyStorm Dungeon/Assets/01.Scripts/Pool/SfxPoolManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class SfxPoolManager : SingletonManager<SfxPoolManager>
+{
+    [SerializeField] private SfxPool sfxPrefab;
+    [SerializeField] private int poolSize = 10;
+
+    private IObjectPool<SfxPool> pool;
+    public IObjectPool<SfxPool> SfxPool
+    {
+        get
+        {
+            if (pool == null) 
+            {
+                pool = new ObjectPool<SfxPool>(
+                    CreateSfx, OnTakeFromPool, OnReturnToPool, null, 
+                    defaultCapacity: poolSize
+                    );
+            }
+            return pool;
+        }
+    }
+
+    public SfxPool GetObject()
+    {
+        return pool.Get();
+    }
+    public void ReturnObject(SfxPool sfx)
+    {
+        pool.Release(sfx);
+    }
+
+    private SfxPool CreateSfx()
+    {
+        var sfx = Instantiate(sfxPrefab, transform);
+        sfx.gameObject.SetActive(false);
+        return sfx;
+    }
+
+    private void OnTakeFromPool(SfxPool sfx)
+    {
+        sfx.gameObject.SetActive(true);
+    }
+
+    private void OnReturnToPool(SfxPool sfx)
+    {
+        sfx.gameObject.SetActive(false);
+    }
+}
