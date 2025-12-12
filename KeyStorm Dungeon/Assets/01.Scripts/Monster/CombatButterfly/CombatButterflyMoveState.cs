d9e8048a14ca0d2065b53129245a4209f876b301
Hagill <youmu12589@gmail.com>
2025-12-12 19:13:52 +0900

    feat: CombatButterfly IsDie 애니메이션 정상화, 이동 방향에 따른 스프라이트 수정중

--- KeyStorm Dungeon/Assets/01.Scripts/Monster/CombatButterfly/CombatButterflyMoveState.cs
+++ KeyStorm Dungeon/Assets/01.Scripts/Monster/CombatButterfly/CombatButterflyMoveState.cs
@@ -1,9 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CombatButterflyMoveState : MonsterMoveState
 {
+
+    private const string ANIM_PARAM_MOVE_X = "MoveX";
+    private const string ANIM_PARAM_MOVE_Y = "MoveY";
+
     public CombatButterflyMoveState(Monster monster, CharacterStateManager<Monster> stateManager) : base(monster, stateManager)
     {
     }
@@ -28,7 +30,11 @@ public class CombatButterflyMoveState : MonsterMoveState
 
         float distanceToPlayer = Vector2.Distance(character.transform.position, playerTransform.position);
 
+        Debug.Log("[CombatButterfly] FixedUpdateState: About to call UpdateAnimation().");
+        UpdateAnimation();
+        Debug.Log("[CombatButterfly] FixedUpdateState: UpdateAnimation() called.");
         UpdateMovement(distanceToPlayer);
+        Debug.Log("[CombatButterfly] FixedUpdateState 호출 종료.");
     }
 
     public override void ExitState()
@@ -39,5 +45,25 @@ public class CombatButterflyMoveState : MonsterMoveState
     public override bool UseFixedUpdate()
     {
         return base.UseFixedUpdate();
+    }
+
+    public void UpdateAnimation()
+    {
+        Debug.Log("--- [CombatButterfly] UpdateAnimation 호출 시작 ---");
+        if (character.Animator == null || playerTransform == null) return;
+        Debug.Log("--- [CombatButterfly] UpdateAnimation: 모든 참조 유효성 검증 통과 ---");
+        Vector2 monsterPos = character.transform.position;
+        Vector2 playerPos = playerTransform.position;
+
+        Vector2 desiredMoveDirection = (playerPos - monsterPos).normalized;
+
+        float moveX = desiredMoveDirection.x;
+        float moveY = desiredMoveDirection.y;
+
+        Debug.Log($"UpdateAnimation: MoveX = {moveX}, MoveY = {moveY}");
+
+        character.Animator.SetFloat(ANIM_PARAM_MOVE_X, moveX);
+        character.Animator.SetFloat(ANIM_PARAM_MOVE_Y, moveY);
+        Debug.Log("--- [CombatButterfly] UpdateAnimation 호출 종료 ---");
     }
 }
