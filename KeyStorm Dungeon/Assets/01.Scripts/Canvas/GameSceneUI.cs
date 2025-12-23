c4e96a44c6c2afbcf0868fd60e9c5fc868f23c02
PeacefulKim <corbata@naver.com>
2025-12-23 21:12:33 +0900

    feat: 간략한 UI
    위가 coin, 두번째 줄이 bombCount입니다

---
+++ KeyStorm Dungeon/Assets/01.Scripts/Canvas/GameSceneUI.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using TMPro;
+
+public class GameSceneUI : MonoBehaviour
+{
+    [SerializeField] private PlayerInventory inventory;
+    [SerializeField] private PlayerAttack attack;
+
+    [SerializeField] private TextMeshProUGUI coinTxt;
+    [SerializeField] private TextMeshProUGUI bombTxt;
+    [SerializeField] private TextMeshProUGUI ammoTxt;
+
+    private void Start()
+    {
+        if (inventory == null || attack == null)
+        {
+            var player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null)
+            {
+                if (inventory == null) inventory = player.GetComponent<PlayerInventory>();
+                if (attack == null) attack = player.GetComponent<PlayerAttack>();
+            }
+        }
+    }
+    void Update()
+    {
+        coinTxt.text = inventory.gold.ToString();
+        bombTxt.text = inventory.bombCount.ToString();
+        ammoTxt.text = attack.Ammo.ToString() + "/" + attack.MaxAmmo.ToString();
+    }
+}
