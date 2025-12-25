efea59a365b734a81b642d6c0c74093808ba3019
PeacefulKim <corbata@naver.com>
2025-12-25 12:19:07 +0900

    feat: 체력 UI

---
+++ KeyStorm Dungeon/Assets/01.Scripts/Items/UI/HealthUI.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private Transform heartContainer;
+    [SerializeField] private Image heartPrefab;
+
+    [Header("Sprites")]
+    [SerializeField] private Sprite fullHeart;
+    [SerializeField] private Sprite halfHeart;
+    [SerializeField] private Sprite emptyHeart;
+
+    private List<Image> hearts = new List<Image>();
+
+    private int maxHp;
+    private int hp;
+
+    public void SetMaxHp(int newMaxHp)
+    {
+        maxHp = Mathf.Max(0, newMaxHp);
+
+        int needHeartCount = Mathf.CeilToInt(maxHp / 2f);
+        RebuildHearts(needHeartCount);
+
+        hp = Mathf.Clamp(hp, 0, maxHp);
+        Refresh();
+    }
+
+    public void SetHp(int newHp)
+    {
+        hp = Mathf.Clamp(newHp, 0, maxHp);
+        Refresh();
+    }
+
+    private void RebuildHearts(int count)
+    {
+        for (int i = hearts.Count - 1; i >= count; i--)
+        {
+            if (hearts[i] != null) Destroy(hearts[i].gameObject);
+            hearts.RemoveAt(i);
+        }
+
+        while (hearts.Count < count)
+        {
+            Image img = Instantiate(heartPrefab, heartContainer);
+            hearts.Add(img);
+        }
+    }
+
+    private void Refresh()
+    {
+        for (int i = 0; i < hearts.Count; i++)
+        {
+            int heartStart = i * 2;
+            int remaining = hp - heartStart;
+
+            if (remaining >= 2) hearts[i].sprite = fullHeart;
+            else if (remaining == 1) hearts[i].sprite = halfHeart;
+            else hearts[i].sprite = emptyHeart;
+        }
+    }
+}
