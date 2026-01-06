9437f5d5191d91fffbec406b4a0d0c52e95ab1c3
Hagill <youmu12589@gmail.com>
2026-01-06 21:19:17 +0900

    fix: 넣은적 없는 using문 삭제

--- KeyStorm Dungeon/Assets/01.Scripts/Monster/Boss/01.Wood/WoodMoveState.cs
+++ KeyStorm Dungeon/Assets/01.Scripts/Monster/Boss/01.Wood/WoodMoveState.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static ConstValue;
-using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class WoodMoveState : MonsterMoveState
 {
