27a93952a9ce79a2615077887178bd758f88d935
youchan97 <151013695+youchan97@users.noreply.github.com>
2026-01-06 12:42:56 +0900

    Chore : 슬라임 에셋 추가

--- KeyStorm Dungeon/Assets/01.Scripts/ScriptableObject/Skills/DynamiteData.cs
+++ KeyStorm Dungeon/Assets/01.Scripts/ScriptableObject/Skills/DynamiteData.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class DynamiteData : SkillData
 {
+    public float radius;
+    public float duration;
+
     public override ISkill CreateSkill(PlayerSkill playerSkill)
     {
         return new DynamiteSkill(playerSkill, this);
