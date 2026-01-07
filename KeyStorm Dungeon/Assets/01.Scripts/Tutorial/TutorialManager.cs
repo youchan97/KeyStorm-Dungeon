0fd4a5fe43832987dd3373f083a51d649dda0edf
Moon <moonky7410@gmail.com>
2026-01-07 12:09:05 +0900

    fix: 튜토리얼 수정

--- KeyStorm Dungeon/Assets/01.Scripts/Tutorial/TutorialManager.cs
+++ KeyStorm Dungeon/Assets/01.Scripts/Tutorial/TutorialManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class TutorialManager : MonoBehaviour
 {
@@ -9,6 +11,9 @@ public class TutorialManager : MonoBehaviour
     [SerializeField] private TutorialUI tutorialUI;
     [SerializeField] private bool showOnStart = true;
     [SerializeField] private bool skipIfSeen = true;
+
+    [Header("튜토리얼 표시할 씬")]
+    [SerializeField] private string[] tutorialScenes = { "MainScene" }; // 첫 씬 이름
 
     public UnityEvent onTutorialStart;
     public UnityEvent onTutorialComplete;
@@ -18,14 +23,57 @@ public class TutorialManager : MonoBehaviour
     private bool isTutorialActive = false;
     private PlayerController playerController;
 
+    private const string GAME_STARTED_KEY = "GameStarted";
+
     private void Start()
     {
         playerController = FindObjectOfType<PlayerController>();
 
+        string currentScene = SceneManager.GetActiveScene().name;
         string hasSeenKey = tutorialData.hasSeenKey;
-        isFirstTimePlaying = PlayerPrefs.GetInt(hasSeenKey, 0) == 0;
+        int seenValue = PlayerPrefs.GetInt(hasSeenKey, 0);
+        int gameStarted = PlayerPrefs.GetInt(GAME_STARTED_KEY, 0);
 
-        if (showOnStart && (!skipIfSeen || isFirstTimePlaying))
+        Debug.Log($"현재 씬: {currentScene}");
+        Debug.Log($"게임 시작됨: {gameStarted}");
+
+        // 게임이 이미 시작됐으면 튜토리얼 스킵
+        if (gameStarted == 1)
+        {
+            Debug.Log("게임 진행 중 - 튜토리얼 건너뛰기");
+            tutorialUI.HideTutorial();
+
+            if (playerController != null)
+            {
+                playerController.AllEnable();
+            }
+
+            StartGame();
+            return;
+        }
+
+        // 이미 봤으면 스킵
+        if (seenValue == 1)
+        {
+            Debug.Log("튜토리얼 이미 봄");
+            tutorialUI.HideTutorial();
+
+            if (playerController != null)
+            {
+                playerController.AllEnable();
+            }
+
+            PlayerPrefs.SetInt(GAME_STARTED_KEY, 1);
+            PlayerPrefs.Save();
+
+            StartGame();
+            return;
+        }
+
+        // 처음이면 튜토리얼 시작
+        isFirstTimePlaying = true;
+
+        if (showOnStart)
         {
             ShowTutorial();
         }
@@ -80,11 +128,9 @@ public class TutorialManager : MonoBehaviour
             playerController.AllEnable();
         }
 
-        if (isFirstTimePlaying)
-        {
-            PlayerPrefs.SetInt(tutorialData.hasSeenKey, 1);
-            PlayerPrefs.Save();
-        }
+        PlayerPrefs.SetInt(tutorialData.hasSeenKey, 1);
+        PlayerPrefs.SetInt(GAME_STARTED_KEY, 1);
+        PlayerPrefs.Save();
 
         onTutorialComplete?.Invoke();
         StartGame();
@@ -120,4 +166,33 @@ public class TutorialManager : MonoBehaviour
     {
         return isTutorialActive;
     }
+
+    [ContextMenu("튜토리얼 리셋")]
+    public void ResetTutorial()
+    {
+        PlayerPrefs.DeleteKey(tutorialData.hasSeenKey);
+        PlayerPrefs.DeleteKey(GAME_STARTED_KEY);
+        PlayerPrefs.Save();
+        Debug.Log("튜토리얼 리셋 완료!");
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(TutorialManager))]
+    public class TutorialManagerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            TutorialManager manager = (TutorialManager)target;
+
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button(" 튜토리얼 리셋", GUILayout.Height(30)))
+            {
+                manager.ResetTutorial();
+            }
+        }
+    }
+#endif
 }
