4788068d73d2cd0408fb0e6dee2fa8fe3201e2fe
youchan97 <151013695+youchan97@users.noreply.github.com>
2026-01-16 12:53:24 +0900

    Feat : 플레이어 포탈 오류 수정 및 인벤토리 작업 중
    인벤토리의 경우 해당 아이템의 UI까지는 뜨게 해놓음

--- KeyStorm Dungeon/Assets/01.Scripts/UI/InventoryUi.cs
+++ KeyStorm Dungeon/Assets/01.Scripts/UI/InventoryUi.cs
@@ -1,29 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryUi : MonoBehaviour
 {
     PlayerInventory inventory;
+    [SerializeField] Transform itemSlots;
+    [SerializeField] GameObject itemSlot;
+    List<ItemData> passiveItems = new List<ItemData>();
+    ItemData activeItem;
 
+    private void OnEnable()
+    {
+        SelectItem();
+    }
+
+    private void OnDisable()
+    {
+        RemoveEvent();
+    }
 
     public void SetInventoryUi(PlayerInventory inventory)
     {
         this.inventory = inventory;
-        inventory.OnAddPassiveItem += AddItemImage;
-
+        InitEvent();
     }
 
-    void AddItemImage(ItemData data)
+    void InitEvent()
     {
-        
+        inventory.OnAddPassiveItem += AddPassiveItem;
+        inventory.OnAddActiveItem += AddActiveItem;
     }
 
-
-    void SetImage()
+    void RemoveEvent()
     {
-        if (inventory == null)
+        inventory.OnAddPassiveItem -= AddPassiveItem;
+        inventory.OnAddActiveItem -= AddActiveItem;
+    }
+
+    void AddPassiveItem(ItemData data)
+    {
+        if (passiveItems.Contains(data))
             return;
+
+        passiveItems.Add(data);
+        Image itemImage = Instantiate(itemSlot, itemSlots).GetComponent<Image>();
+        itemImage.sprite = data.iconSprite;
+    }
+
+    void AddActiveItem(ItemData data)
+    {
+        if (activeItem == data)
+            return;
+
+        activeItem = data;
+    }
+
+    void SelectItem(int index = 0)
+    {
+        if (passiveItems.Count == 0 && activeItem == null)
+            return;
+        else if (passiveItems.Count == 0)
+        {
+            ViewItemInfo(activeItem);
+            return;
+        }
+
+        ViewItemInfo(passiveItems[index]);
+
+    }
+
+    void ViewItemInfo(ItemData data)
+    {
+        if (data == null)
+            return;
+
 
         
     }
