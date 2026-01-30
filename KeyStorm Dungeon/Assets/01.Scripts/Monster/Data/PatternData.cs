26c03872bed85764c1c63fe8eba4aa477c8cc6de
Hagill <youmu12589@gmail.com>
2026-01-30 19:06:15 +0900

    chore: 사용하지 않는 스크립트 삭제

--- KeyStorm Dungeon/Assets/01.Scripts/Monster/Data/PatternData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class PatternData : ScriptableObject
-{
-    [Header("패턴 정보")]
-    public string patternName;
-    public float patternCooldown;
-}
